48592db0180982de5079b8b9779853d4e0a40822
John Goddard <johngoddard243@gmail.com>
2016-10-05 15:37:28 -0700

    better lca

---
+++ lib/10chap.rb
@@ -0,0 +1,51 @@
+def is_balanced?(root)
+  return { depth: -1, balanced: true } unless root
+
+  left = is_balanced?(root.left)
+  right = is_balanced?(root.right)
+
+  if(left[:balanced] && right[:balanced] && (left[:depth] - right[:depth]).abs <= 1)
+    return {balanced: true, depth: [left[:depth], right[:depth]].max + 1}
+  end
+
+  {balanced: false, depth: 0}
+end
+
+def check_balance(root)
+  is_balanced?(root)[:balanced]
+end
+
+def least_common_ancestor(node1, node2)
+  parents_1 = Hash.new(false)
+  
+  depth1 = get_depth(node1)
+  depth2 = get_depth(node2)
+
+  if(depth2 > depth1)
+    node1, node2 = node2, node1
+  end
+  
+  i = 0 
+  while i < (depth1 - depth2).abs 
+   i += 1
+   node1 = node1.parent
+  end
+
+  while node1
+    return node1 if node1 == node2
+    node1 = node1.parent 
+    node2 = node2.parent
+  end
+
+  nil 
+end
+
+def get_depth(node)
+  depth = 0
+  
+  while node.parent
+    depth += 1 
+    node = node.parent
+  end
+  depth 
+end
---
+++ spec/10chap_spec.rb
@@ -0,0 +1,69 @@
+require '10chap'
+
+class TreeNode
+  attr_accessor :val, :left, :right, :parent
+
+  def initialize(val, parent = nil, left=nil, right=nil)
+    @val = val
+    @parent = parent
+    @left = left
+    @right = right
+  end
+end
+
+describe '10.1 #check_balanced' do 
+  it 'returns false for an unbalanced tree' do 
+    a = TreeNode.new(3)
+    b = TreeNode.new(1)
+    c = TreeNode.new(4)
+    a.right = b
+    b.right = c
+
+    expect(check_balance(a)).to be(false)
+  end
+
+  it 'returns true for a balanced tree' do 
+    a = TreeNode.new(314)
+    b = TreeNode.new(6)
+    c = TreeNode.new(6)
+    d = TreeNode.new(5)
+    e = TreeNode.new(7)
+    f = TreeNode.new(8)
+
+    a.left = b
+    a.right = c
+    b.left = d
+    b.right = e
+    c.left = f
+  
+    expect(check_balance(a)).to be(true)
+  end
+end
+
+describe '10.4 #least_common_ancestor' do 
+  a = TreeNode.new(10)
+  b = TreeNode.new(6)
+  c = TreeNode.new(15)
+  d = TreeNode.new(5)
+  e = TreeNode.new(7)
+  f = TreeNode.new(8)
+
+  a.left = b
+  b.parent = a
+  a.right = c
+  c.parent = a
+  b.left = d
+  d.parent = b
+  b.right = e
+  e.parent = b
+  c.left = f
+  f.parent = c
+
+  it 'returns the least common ancestor' do
+    expect(least_common_ancestor(d, e).val).to eq(6)
+  end
+
+  it 'returns the lca when the lca is one of the nodes' do
+    expect(least_common_ancestor(a, b).val).to eq(10)
+  end
+end
