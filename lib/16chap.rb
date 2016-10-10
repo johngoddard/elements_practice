9411f0159120ac2bdab0d26acb92ddfbafff7102
John Goddard <johngoddard243@gmail.com>
2016-10-09 17:28:57 -0700

    8 queens

--- lib/16chap.rb
+++ lib/16chap.rb
@@ -1,7 +1,36 @@
+# 16.1: recursively solve towers of Hanoi
+
 def towers_solver(num_rings, pegs, from_peg, to_peg, use_peg)
   return if num_rings == 0
   towers_solver(num_rings - 1, pegs, from_peg, use_peg, to_peg)
   pegs[to_peg] << pegs[from_peg].pop unless pegs[from_peg].empty?
   towers_solver(num_rings - 1, pegs, use_peg, to_peg, from_peg)
 end
+
+# 16.2: recursively come up with all legal ways to place N queens on an N by N board
+def solve_n_queens(n, row = 0, curr_board = [], result = [])
+  result << curr_board if row == n
+  
+  (0...n).each do |col|
+    if(row == 0 || legal_placement?(row, col, curr_board))
+      board = curr_board.dup.concat([[row, col]])
+      solve_n_queens(n, row + 1, board.dup, result)
+    end
+  end
+
+  result
+end
+
+def legal_placement?(row, col, curr_board)
+  curr_board.each do |queen|
+    return false if queen[1] == col
+    
+    slope = (row - queen[0]) / (col - queen[1]).to_f
+    return false if slope == 1 || slope == -1
+  end
+
+  true
+end
+
+
 
--- spec/16chap_spec.rb
+++ spec/16chap_spec.rb
@@ -6,9 +6,20 @@ describe '16.1: #towers_solver' do
     towers_solver(3, pegs, 0, 2, 1)
     expect(pegs).to eq([[],[],[3,2,1]])
   end
+
   it 'solves towers of hanoi for 5 towers' do 
     pegs = [[5,4,3,2,1], [], []]
     towers_solver(5, pegs, 0, 2, 1)
     expect(pegs).to eq([[],[],[5,4,3,2,1]])
+  end
+end
+
+describe '16.2 #solve_n_queens' do 
+  it 'solves n queens for 8 queens' do 
+    expect(solve_n_queens(8).length).to eq(92)
+  end
+
+  it 'solves n queens for 4 queens' do 
+    expect(solve_n_queens(4).length).to eq(2)
   end
 end
