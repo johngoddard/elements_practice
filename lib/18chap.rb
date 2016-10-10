f0f535de1d182a1982e735246f7cce5a51acd453
John Goddard <johngoddard243@gmail.com>
2016-10-10 13:27:21 -0700

    three sum

--- lib/18chap.rb
+++ lib/18chap.rb
@@ -1,4 +1,4 @@
-# Write a function that accepts an array of tasks and assigns them to workers in an optimal way. 
+# 18.1:  Write a function that accepts an array of tasks and assigns them to workers in an optimal way. 
 # Assume there are N workers and 2N tasks. 
 #
 # E.g. [5,2,1,6,4,4] => [[1,6], [2,5], [4,4]], since 4+4 = 88 s the lowest possible max time for a
@@ -14,4 +14,27 @@ def assign_tasks(tasks)
   end
   
   res
+end
+
+# 18.4 -- three sum: Accept an array and a target value. Return true if any 3 numbers in the array
+# add up to the target. You can use the same number more than once
+
+def three_sum(arr, target)
+  arr.each_with_index do |el, i|
+    return true if two_sum(arr, target, i)
+  end
+
+  false
+end
+
+def two_sum(arr, target, idx)
+  els = Set.new
+  arr.each_with_index{|el, i| els << el }
+  target = target - arr[idx]
+  
+  arr.each_with_index do |el, i|
+    return true if els.include?(target - el)
+  end
+
+  false
 end
--- spec/18chap_spec.rb
+++ spec/18chap_spec.rb
@@ -5,3 +5,21 @@ describe '18.1: assign tasks' do
     expect(assign_tasks([5,2,1,6,4,4])).to eq([[1,6], [2,5], [4,4]])
   end
 end
+
+describe '18.4 three_sum' do 
+  it 'returns true for an array with 3 nums that add up to a target' do 
+    expect(three_sum([1,2,3], 6)).to eq(true)
+  end 
+
+  it 'will use repeat numbers' do 
+    expect(three_sum([5], 15)).to eq(true)
+  end
+  
+  it 'works for no repeats' do 
+    expect(three_sum([11, 2, 5, 7, 3], 12)).to eq(true)
+  end
+
+  it 'returns false when there is no 3 sum' do 
+    expect(three_sum([11, 2, 5, 7, 3], 22)).to eq(false)
+  end
+end
